b4f6ca8bddd37442ba858b41152906c5d103247a
Steffen Vandegaer <steffenvandegaer@hotmail.com>
2017-11-08 23:36:37 +0100

    loginsysteem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,19 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { AuthProvider } from '../providers/auth/auth';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA43tuYz9kTRECR4Du1mnycRbsAblaSGAg",
+  authDomain: "diasports-67224.firebaseapp.com",
+  databaseURL: "https://diasports-67224.firebaseio.com",
+  projectId: "diasports-67224",
+  storageBucket: "diasports-67224.appspot.com",
+  messagingSenderId: "529704432993"
+};
 
 @NgModule({
   declarations: [
@@ -21,7 +34,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -34,7 +49,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AuthProvider
   ]
 })
 export class AppModule {}
